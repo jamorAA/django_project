6b5755a5cd9805203159f3141263d3a2addccc59
ainur <4younghope4@gmail.com>
2025-12-18 01:02:16 +0300

    done? 9

--- app/settings.py
+++ app/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = 'django-insecure-ld)8k$gfoow^#9-6mm@palg6l9d)&l!5ng#m2@=msxhq353g#2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['django-project-jamorant.amvera.io', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['http://django-project-jamorant.amvera.io', 'https://django-project-jamorant.amvera.io']
 
 
 # Application definition
