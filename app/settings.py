cc9023d030875338a5d004da0bb50d28751418e3
ainur <4younghope4@gmail.com>
2025-12-18 12:24:12 +0300

    Revert "final commit"
    This reverts commit d4f76ca663a43179973006ce64769519731f87fd.

--- app/settings.py
+++ app/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ld)8k$gfoow^#9-6mm@palg6l9d)&l!5ng#m2@=msxhq353g#2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['django-project-jamorant.amvera.io', '127.0.0.1']
 CSRF_TRUSTED_ORIGINS = ['http://django-project-jamorant.amvera.io', 'https://django-project-jamorant.amvera.io']
